3faecf0751f6a273c4f23a8bc61be17eab08f260
flook25 <flook11223344@gmail.com>
2025-05-16 21:09:33 +0700

    Add special offer banner and update home page

--- app_books/views.py
+++ app_books/views.py
@@ -1,9 +1,32 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+BOOKS = [
+    {
+        "id": 1,
+        "title": "Python Programming 101",
+        "author": "John Doe",
+        "price": 350,
+        "img": "https://cdn-icons-png.flaticon.com/128/29/29302.png"
+    },
+    {
+        "id": 2,
+        "title": "Django for Beginners",
+        "author": "Jane Smith",
+        "price": 420,
+        "img": "https://cdn-icons-png.flaticon.com/128/29/29302.png"
+    },
+    {
+        "id": 3,
+        "title": "R Programming for Data Science",
+        "author": "Jane Miller",
+        "price": 390,
+        "img": "https://cdn-icons-png.flaticon.com/128/29/29302.png"
+    }
+]
+
 def books(request):
-    return render(request, 'app_books/books.html')
+    return render(request, 'app_books/books.html', {'books': BOOKS})
 
 def book(request, book_id):
-    return render(request, 'app_books/book.html', {'book_id': book_id})
+    book = next((b for b in BOOKS if b["id"] == book_id), None)
+    return render(request, 'app_books/book.html', {'book': book})
