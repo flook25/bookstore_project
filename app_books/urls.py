b393c852f675755b6b0a09c6bd88af11b04b14e9
flook25 <flook11223344@gmail.com>
2025-05-16 19:33:22 +0700

    Update books.html and project templates

--- app_books/urls.py
+++ app_books/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.books, name='books'),
-    path('books/', views.books, name='book')
+    path('<int:book_id>/', views.book, name='book'),
 ]
--- app_books/views.py
+++ app_books/views.py
@@ -5,4 +5,5 @@ from django.shortcuts import render
 def books(request):
     return render(request, 'app_books/books.html')
 
-
+def book(request, book_id):
+    return render(request, 'app_books/book.html', {'book_id': book_id})
